4fcea787617d9a820ba306aaa08cd7bb8b0d9c8d
Niamat Marjan <86093827+NimBuzz01@users.noreply.github.com>
2023-05-22 14:30:06 +0530

    Header re-design

--- src/components/Accelerators/ConnectToEmr.tsx
+++ src/components/Accelerators/ConnectToEmr.tsx
@@ -1,16 +1,8 @@
 import { Container } from "@mui/material";
-import { Heading, CodeEditor } from "../Common";
-import { CONNECT_TO_EMR_ABOUT_URL } from "../Configs/Constants";
 
 export const ConnectToEmr = () => {
   return (
     <Container maxWidth="xl">
-      <Heading
-        heading="Connect to EMR"
-        description="Connect with your EMR system"
-        url={CONNECT_TO_EMR_ABOUT_URL}
-      ></Heading>
-      <CodeEditor />
     </Container>
   );
 };
