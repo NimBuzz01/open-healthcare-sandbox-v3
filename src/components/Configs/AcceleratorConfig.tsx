b679fd2fa42a795e9b5226ae5981228cee12cda6
Niamat Marjan <86093827+NimBuzz01@users.noreply.github.com>
2023-05-22 22:52:01 +0530

    code correction and adjustments

--- src/components/Configs/AcceleratorConfig.tsx
+++ src/components/Configs/AcceleratorConfig.tsx
@@ -29,7 +29,6 @@ export const items = [
     path: "/",
     icon: <LocalFireDepartmentOutlinedIcon sx={{ width: 26, height: 26 }} />,
     component: <FhirApis />,
-    samples: <ApiSamples />,
   },
   {
     label: "HL7V2 To FHIR",
